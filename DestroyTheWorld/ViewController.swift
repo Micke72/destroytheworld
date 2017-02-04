6ba6c063f055ecf91f77da1e735f7fe4d48390bf
Jan-Michael Junell <Micke72@MacBook-Pro.local>
2017-02-04 23:19:06 +0100

    Added destroy print

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello world!!")
+        print("destroyed"	)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,3 +26,4 @@ class ViewController: UIViewController {
 
 }
 
+	
